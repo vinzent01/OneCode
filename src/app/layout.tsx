089c0df0480ad458a17269e6a48a418839d22892
vinzent01 <vinzent0101@gmail.com>
2025-05-21 14:44:23 -0300

    changed website title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,6 +26,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <title>OneCode</title>
+      </head>
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         <Header />
         {children}
